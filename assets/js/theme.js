e2619357d18929764884dced34fcbfc39f8f53a9
prettypet2137 <prettypet2137@gmail.com>
2022-11-21 22:23:40 +0900

    css finished

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+	$('.qa-opener').off('click').on('click', function(){
+		// if(!$(this).hasClass('opened')) {
+		// 	$(this).next('.qa-popup').slideDown();
+		// 	$(this).addClass('opened');
+		// } else {
+		// 	$(this).next('.qa-popup').slideUp();
+		// 	$(this).removeClass('opened');
+		// }
+	});
+});
