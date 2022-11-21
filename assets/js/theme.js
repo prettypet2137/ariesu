554b9c987ec7a58d394d49361ee3503173d23fb1
prettypet2137 <prettypet2137@gmail.com>
2022-11-22 01:22:51 +0900

    3config.rb updated

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,11 @@
 jQuery(function($){
 	$('.qa-opener').off('click').on('click', function(){
-		// if(!$(this).hasClass('opened')) {
-		// 	$(this).next('.qa-popup').slideDown();
-		// 	$(this).addClass('opened');
-		// } else {
-		// 	$(this).next('.qa-popup').slideUp();
-		// 	$(this).removeClass('opened');
-		// }
+		if(!$(this).hasClass('opened')) {
+			$(this).next('.qa-popup').slideDown();
+			$(this).addClass('opened');
+		} else {
+			$(this).next('.qa-popup').slideUp();
+			$(this).removeClass('opened');
+		}
 	});
 });
